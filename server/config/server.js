06299aa3b63ccca50997a980377c373a1b10a820
Sanyam Sharma <84903641+Sanyam17-beep@users.noreply.github.com>
2023-07-19 21:20:59 +0530

    Update server.js

--- server/config/server.js
+++ server/config/server.js
@@ -1,5 +1,10 @@
 module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   url:env("RENDER_EXTERNAL_URL"),
+  admin:{
+    url:'/dashboard',
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
